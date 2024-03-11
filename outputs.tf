aff2c71ea35a8120dcb00354b2bfc460985b12d4
ravi <rprakash133@gmail.com>
2024-03-11 15:51:26 +0530

    latest comit

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,8 @@ output "aws_codepipeline_arn" {
   description = "aws codepipeline project arn"
 }
 
-# output "aws_codebuild_project_arn" {
-#   value = module.codebuild.aws_codebuild_project_arn.this.arn
-#   description = "aws codebuild project arn"
+output "aws_codebuild_project_arn" {
+  value = module.codebuild.aws_codebuild_project_arn.this.arn
+  description = "aws codebuild project arn"
 
-#   }
+  }
