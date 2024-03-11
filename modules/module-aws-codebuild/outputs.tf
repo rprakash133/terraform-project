bb42985b812003d9874a257a0ba66215c595cafd
ravi <rprakash133@gmail.com>
2024-03-11 16:36:48 +0530

    latest comit

--- modules/module-aws-codebuild/outputs.tf
+++ modules/module-aws-codebuild/outputs.tf
@@ -1,4 +1,4 @@
-# output "aws_codebuild_project_arn" {
-#   value       = aws_codebuild_project.cicd-pro.arn
-#   description = "aws codebuild project arn"
-# }
+output "aws_codebuild_project_arn" {
+  value       = aws_codebuild_project.cicd-pro.arn
+  description = "aws codebuild project arn"
+}
